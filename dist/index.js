6b119f0dcd5703d1b7b25744e941233bae4fa210
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2023-06-12 14:16:02 +0200

    🐐

--- dist/index.js
+++ dist/index.js
@@ -71,10 +71,6 @@ exports.INPUT = void 0;
 exports.INPUT = {
     APP_KEY: 'app_key',
     GITHUB_BRANCH: 'github_branch'
-    // CLOUDFLARE_WORKSITE_ZONE_ID: 'cloudflare_worksite_zone_id',
-    // CLOUDFLARE_TRUSTUP_IO_ZONE_ID: 'cloudflare_trustup_io_zone_id',
-    // DOPPLER_SERVICE_TOKEN_PRODUCTION: 'doppler_service_token_production',
-    // DOPPLER_SERVICE_TOKEN_STAGING: 'doppler_service_token_staging'
 };
 
 
